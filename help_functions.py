f2b7241398ee2390ee0ae2e71be79bb2ebb20d41
DbINYA <ALgoRITm2608@yandex.ru>
2022-12-01 13:27:14 +0300

    Update help_functions.py

--- help_functions.py
+++ help_functions.py
@@ -5,42 +5,42 @@ import os
 
 
 def my_scaler():
-	scaler = pickle.load(open('eda_preprocessing_models/scaler.pkl','rb'))
-	return scaler
+    scaler = pickle.load(open('eda_preprocessing_models/scaler.pkl','rb'))
+    return scaler
 
 
 def model_depth():
-	model = pickle.load(open('eda_preprocessing_models/forest_depth.pkl','rb'))
-	return model
+    model = pickle.load(open('eda_preprocessing_models/forest_depth.pkl','rb'))
+    return model
 
 
 def model_width():
-	model = pickle.load(open('eda_preprocessing_models/forest_width.pkl','rb'))
-	return model
+    model = pickle.load(open('eda_preprocessing_models/forest_width.pkl','rb'))
+    return model
 
 
 def models_predict(data):
-	model_for_depth = model_depth()
-	model_for_width = model_width()
-	depth = model_for_depth.predict(data)
-	width = model_for_width.predict(data)
-	if len(data) == 1:
-		return [round(depth[0], 2), round(width[0], 2)]
-	li = [[round(depth[i], 2), round(width[i], 2)] for i in range(len(data))]
-	return li
+    model_for_depth = model_depth()
+    model_for_width = model_width()
+    depth = model_for_depth.predict(data)
+    width = model_for_width.predict(data)
+    if len(data) == 1:
+	    return [round(depth[0], 2), round(width[0], 2)]
+    li = [[round(depth[i], 2), round(width[i], 2)] for i in range(len(data))]
+    return li
 
 
 def preporations(data):
-	scaler = my_scaler()
-	try:
+    scaler = my_scaler()
+    try:
         scaler_data = scaler.transform(data)
         return models_predict(scaler_data)
-	except:
+    except:
 	    pass
 
 
 def predict_big_data(name):
-	try:
+    try:
 	    df = pd.read_csv(name)
 	    scaler = my_scaler()
 	    scaler_data = scaler.transform(df)
@@ -50,5 +50,5 @@ def predict_big_data(name):
 	    path_for_user = '/'.join(name.split('/')[-2:-1]) + '/pred_data.csv'
 	    pred_df.to_csv(new_path, index=False)
 	    return path_for_user
-	except:
-		pass
+    except:
+	    pass
